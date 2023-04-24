ecd04e6d19b40c9d11d8870205dbb9920ca2fa89
herrluk <herrluk@example.com>
2023-04-24 20:19:28 +0800

    第一天

---
+++ models/captcha.go
@@ -0,0 +1,15 @@
+package models
+
+import "github.com/mojocn/base64Captcha"
+
+// 获取验证码
+func MakeCapctha() {
+
+}
+
+// 创建 store
+var store = base64Captcha.DefaultMemStore
+
+func VerifyCapctha() {
+
+}
